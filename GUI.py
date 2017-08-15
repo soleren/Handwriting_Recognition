ee309fb04dc7d09b897a89f5343dbbb367f7708a
Den <Qw1sd3cv>
2017-08-15 20:51:24 +0300

    f

--- GUI.py
+++ GUI.py
@@ -10,16 +10,17 @@ import NeuralNet
 class Paint(Frame):
     width = 200
     height = 200
-    # alphabet = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ь','Ы','Э','Ю','Я','0','1','2','3','4','5','6','7','8','9']
-    alphabet = ['0','1','2','3','4']
+    alphabet = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ь','Ы','Э','Ю','Я','0','1','2','3','4','5','6','7','8','9']
+    # alphabet = ['0','1','2','3','4']
     learned = []
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
         self.hnodes = 400
-        self.learning_rate = 0.001
+        self.learning_rate = 0.0005
         self.nnet = NeuralNet.NeuralNet((self.width * self.height), self.hnodes,len(self.alphabet),self.learning_rate)
+        # self.nnet = NeuralNet.NeuralNet(2, 2,2,self.learning_rate)
 
         self.setUI()
         self.brush_size = 8
@@ -35,13 +36,11 @@ class Paint(Frame):
         self.canv.bind("<B1-Motion>", self.draw)
         self.canv.bind("<Button-3>", self.clear)
 
-        learn_label = Label(self, text="-" * 10 + " Learn " + "-" * 10)
+        learn_label = Label(self, text="-" * 5 + " Add training examples " + "-" * 5)
         learn_label.grid(row=0, column=5, columnspan=5)
 
         learn_first = Label(self, text="1. Write a letter       ")
         learn_first.grid(row=1, column=5,columnspan=2)
-        learn_first = Label(self, text="rclick to clear")
-        learn_first.grid(row=1, column=7,columnspan=2)
 
         learn_second = Label(self, text="2. Select the letter   ")
         learn_second.grid(row=2, column=5,columnspan=2)
@@ -51,20 +50,24 @@ class Paint(Frame):
 
         learn_third_label = Label(self, text="3. Push the button  ")
         learn_third_label.grid(row=3, column=5,columnspan=2)
-        learn_third_btn = Button(self, text="Learn", width=8, command=lambda: self.learn_btn(self))
+        learn_third_btn = Button(self, text="Add", width=8, command=lambda: self.add_btn(self))
         learn_third_btn.grid(row=3, column=7, padx=5,columnspan=3)
 
-        recognize_label = Label(self, text="-" * 10 + " Recognize " + "-" * 10)
-        recognize_label.grid(row=4, column=5, columnspan=5)
+        separator_label = Label(self, text="-" * 34)
+        separator_label.grid(row=4, column=5, columnspan=5)
 
-        recognize_first = Label(self, text="4. Write a letter        ")
-        recognize_first.grid(row=5, column=5,columnspan=2)
 
-        recognize_second_label = Label(self, text="5. Push the button  ")
-        recognize_second_label.grid(row=6, column=5,columnspan=2)
+        learn_btn = Button(self, text="Learn!", width=8, command=lambda: self.learn())
+        learn_btn.grid(row=5, column=5, padx=5,columnspan=2)
 
         recognize_second_btn = Button(self, text="Recognize", width=8,command=lambda: self.recognize(self))
-        recognize_second_btn.grid(row=6, column=7, padx=5,columnspan=3)
+        recognize_second_btn.grid(row=5, column=7, padx=5,columnspan=3)
+
+        load_btn = Button(self, text="Load", width=8, command=lambda: self.load())
+        load_btn.grid(row=6, column=5, padx=5,columnspan=2)
+
+        save_btn = Button(self, text="Save", width=8,command=lambda: self.save())
+        save_btn.grid(row=6, column=7, padx=5,columnspan=3)
 
         learned_label = Label(self, text="-" * 32 + " Learned " + "-" * 32)
         learned_label.grid(row=11, column=0, columnspan=10)
@@ -72,14 +75,14 @@ class Paint(Frame):
         result_label = Label(self, text="-" * 33 + " Result " + "-" * 33)
         result_label.grid(row=20, column=0, columnspan=10)
 
-        col = 0
-        row = 0
-        for i in range(len(self.alphabet)):
-            if i % 6 == 0 and i != 0:
-                col+=1
-                row=0
-            Label(self, text="%s:%1.3f" % (self.alphabet[i],0)).grid(row=21+row, column=col+1,sticky=W)
-            row+=1
+        # col = 0
+        # row = 0
+        # for i in range(len(self.alphabet)):
+        #     if i % 6 == 0 and i != 0:
+        #         col+=1
+        #         row=0
+        #     Label(self, text="%s:%1.3f" % (self.alphabet[i],0)).grid(row=21+row, column=col+1,sticky=W)
+        #     row+=1
 
     def draw(self,event):
         self.canv.create_oval(event.x - self.brush_size,event.y - self.brush_size,
@@ -89,30 +92,28 @@ class Paint(Frame):
     def clear(self, event):
         self.canv.delete("all")
 
-    def learn_btn(self, widget):
-
+    def add_btn(self, widget):
         x = self.winfo_rootx()
         y = self.winfo_rooty()
         x1 = x + self.width
         y1 = y + self.height
-        # self.li = ImageGrab.grab().crop((x, y, x1, y1)).save(os.getcwd()+"\\img\\" + str(random.randint(1, 1000000000))+".JPG")
         im = ImageGrab.grab().crop((x, y, x1, y1))
-
         numimage = np.dot(np.array(im),[0.299, 0.587, 0.114]).ravel()
-        # im = Image.fromarray(numimage)
-        # im.show()
-        im.save(os.getcwd()+"\\img\\" + str(random.randint(1, 1000000000))+".JPG")
+        # im.save(os.getcwd()+"\\img\\" + str(random.randint(1, 1000000000))+".JPG")
         scaled_image = (numimage / 255 * 0.98) + 0.01
-        # print(list(scaled_image))
-        # im = Image.fromarray(scaled_image)
-        # im.show()
+        a_str = ','.join(str(x) for x in scaled_image)
+        output = [0.99 if self.curval == x else 0.01 for x in self.alphabet ]
+        o_str = ','.join(str(x) for x in output)
+
+        f = open(os.getcwd()+"\\data.txt",'a')
+        f.write(self.curval+','+o_str+','+a_str+'\n')
+        f.close()
+
         if self.curval not in self.learned:
             self.learned.append(self.curval)
 
-        output = [0.99 if self.curval == x else 0.01 for x in self.alphabet ]
-        print(output)
-        for i in range(40):
-            self.nnet.train(scaled_image,output)
+
+
         r = 12
         c = 0
         for i in range(len(self.learned)):
@@ -121,22 +122,33 @@ class Paint(Frame):
                 c = 0
             Label(self, text="%s " % self.learned[i]).grid(row=r, column=c)
             c+=1
+        self.clear(None)
 
+    def learn(self):
+        # try:
+            for i in range(10):
+                f = open(os.getcwd()+"\\data.txt",'r')
+                for line in f:
+                    label = line.split(',')[0]
+                    output = list(map(float,line.split(',')[1:len(self.alphabet)+1]))
+                    data = list(map(float,line.split(',')[len(self.alphabet) + 1:]))
+                    self.nnet.train(data,output)
+                print(i)
+        # except:
+        #     print("Не удалось открыть файл")
+        # finally:
+        #     f.close()
 
     def recognize(self,widget):
         x = self.winfo_rootx()
         y = self.winfo_rooty()
         x1 = x + self.width
         y1 = y + self.height
-        # self.rl = ImageGrab.grab().crop((x, y, x1, y1)).save(os.getcwd()+"\\img\\" + str(random.randint(1, 1000000000))+".JPG")
-        # if self.li == self.rl:
-        #     print("!!!!")
         im = ImageGrab.grab().crop((x, y, x1, y1))
-        # im.show()
         numimage = np.dot(np.array(im),[0.299, 0.587, 0.114]).ravel()
         scaled_image = (numimage / 255 * 0.98) + 0.01
         result = self.nnet.work(scaled_image)
-        print(result)
+
         col = 0
         row = 0
         for i in range(len(result)):
@@ -146,7 +158,29 @@ class Paint(Frame):
             Label(self, text="%s:%3.2f " % (self.alphabet[i],result[i]*100)).grid(row=21+row, column=col+1,sticky=W)
             row+=1
 
+    def save(self):
+        f = open(os.getcwd()+"\\weights.txt",'w')
+        # wih = '\n'.join([','.join(str(number) for number in line) for line in self.nnet.wih])
+        # who = '\n'.join([','.join(str(number) for number in line) for line in self.nnet.who])
+        wih =  ','.join(str(x) for x in self.nnet.wih.ravel())
+        who =  ','.join(str(x) for x in self.nnet.who.ravel())
+        f.write(wih+','+who)
+        print(self.nnet.wih)
+        print(self.nnet.who)
+        print(self.nnet.who.shape)
+        f.close()
 
+    def load(self):
+        # try:
+            f = open(os.getcwd()+"\\weights.txt",'r')
+            weights = f.readline().split(',')
+            self.nnet.wih = np.array(weights[ : self.nnet.wih.shape[0] * self.nnet.wih.shape[1]]).reshape(self.nnet.wih.shape)
+            self.nnet.who = np.array(weights[self.nnet.who.shape[0] * self.nnet.who.shape[1]: ]).reshape(self.nnet.who.shape)
+
+        # except:
+        #     print("Не удалось открыть файл")
+        # finally:
+        #     f.close()
     def get_value(self,value):
         self.curval = value
 
--- NeuralNet.py
+++ NeuralNet.py
@@ -69,10 +69,6 @@ class NeuralNet:
 
 if __name__ == "__main__":
 
-    # input_nodes = 400
-    # hidden_nodes = 100
-    # output_nodes =  5
-    # learning_rate = 0.4
     input_nodes = 40000
     hidden_nodes = 400
     output_nodes =  5
@@ -131,65 +127,4 @@ if __name__ == "__main__":
     print(n.work(scaled4).ravel())
 
 
-    # for i in range(100):
-    #     n.train([0.2,0.2,0.2], [0.2,0.2,0.2])
-    #     n.train([0.9,0.9,0.9], [0.9,0.9,0.9])
-    # print(n.work([0.3,0.3,0.3]))
-    # print(n.work([0.7,0.7,0.7]))
-    # print(n.work([0.1,0.1,0.1]))
-
-
-    # # load the mnist training data CSV file into a list
-    # training_data_file = open("mnist_train.csv", 'r')
-    # training_data_list = training_data_file.readlines()
-    # training_data_file.close()
-    # # train the neural network
-    # # epochs is the number of times the training data set is used for training
-    # epochs = 5
-    # for e in range(epochs):
-    #     # go through all records in the training data set
-    #     for record in training_data_list:
-    #         # split the record by the ',' commas
-    #         all_values = record.split(',')
-    #         # scale and shift the inputs
-    #         inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    #         # create the target output values (all 0.01, except the desired label which is 0.99)
-    #         targets = np.zeros(output_nodes) + 0.01
-    #
-    #         # all_values[0] is the target label for this record
-    #         targets[int(all_values[0])] = 0.99
-    #         n.train(inputs, targets)
-    #     pass
-    # pass
-    # # # load the mnist test data CSV file into a list
-    # test_data_file = open("mnist_test.csv", 'r')
-    # test_data_list = test_data_file.readlines()
-    # test_data_file.close()
-    # # test the neural network
-    # # scorecard for how well the network performs, initially empty
-    # scorecard = []
-    # # go through all the records in the test data set
-    # for record in test_data_list:
-    # # split the record by the ',' commas
-    #     all_values = record.split(',')
-    #     # correct answer is first value
-    #     correct_label = int(all_values[0])
-    #     # scale and shift the inputs
-    #     inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    #     # query the network
-    #     outputs = n.work(inputs)
-    #     # the index of the highest value corresponds to the label
-    #     label = np.argmax(outputs)
-    #     # append correct or incorrect to list
-    #     if (label == correct_label):
-    #     # network's answer matches correct answer, add 1 to scorecard
-    #         scorecard.append(1)
-    #     else:
-    #     # network's answer doesn't match correct answer, add 0 to scorecard
-    #         scorecard.append(0)
-    #     pass
-    # pass
-    # # calculate the performance score, the fraction of correct answers
-    # scorecard_array = np.asarray(scorecard)
-    # print ("performance = ", scorecard_array.sum() /   scorecard_array.size)
 
